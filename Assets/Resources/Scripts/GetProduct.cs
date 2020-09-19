9f67b63f79d675e334732e8e4f40f476a7a506eb
Vithushan Mahendran <vithushan.mahendran@students.fhnw.ch>
2020-09-19 18:22:33 +0200

    added database with our target products

---
+++ Assets/Resources/Scripts/GetProduct.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GetProduct : MonoBehaviour
+{
+     public string productName ="";
+     public Product product;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(API.GetProduct("104400100000").id);
+        //Debug.Log(API.GetProduct("104400100000").name);
+        Debug.Log("Hallo Welt");
+        product = API.GetProduct("104400100000");
+        productName = product.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextMeshPro infoPro = GetComponent<TextMeshPro>();
+        string content = "";
+        content += productName + "\n";
+        content += product.nutrition_facts.standard.nutrients[0].quantity + " kcal";
+        infoPro.SetText(content + "\n");
+    }
+}
