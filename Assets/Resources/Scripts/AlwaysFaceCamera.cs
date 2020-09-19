8e57fa662d1336290b4178329186924c098dfe01
David Schlebusch <david@Davids-MacBook-Pro-2.local>
2020-09-19 10:51:34 +0200

    Added always facing camera.

---
+++ Assets/Resources/Scripts/AlwaysFaceCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AlwaysFaceCamera : MonoBehaviour
+{
+    Camera arCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = arCamera.transform.rotation;
+    }
+}
